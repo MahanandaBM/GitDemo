011fad053eb46f7202b0407b6f3115ab2b76b450
MahanandaBM <mahananda_bm@yahoo.co.in>
2020-03-10 19:13:37 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browser()
+	{
+		System.out.println("Browser");
+	}
+	
+	//@Test
+	public void moblie()
+	{
+		System.out.println("Mobile");
+	}
+
+}
